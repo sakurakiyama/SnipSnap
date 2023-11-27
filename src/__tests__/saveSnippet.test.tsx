cecc37c92270a53c981dad47c3c5f5e693102dbd
sakurakiyama <sakura.bowden@gmail.com>
2023-11-27 16:15:38 -0500

    start github workflow

---
+++ src/__tests__/saveSnippet.test.tsx
@@ -0,0 +1,5 @@
+/*
+[] should save the code snippet as a jpg if jpg is selected and download attribute is supported
+[] should save the code snippet as a png if png is selected and download attribute is supported
+[] should open the canvas in a new window if download not supported
+*/
